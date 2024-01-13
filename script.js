be5acae32dca3a8dbfc9773c9a6050083bb335e9
Andrew Butler <abutler911@gmail.com>
2024-01-12 23:01:00 -0700

    fight function

--- script.js
+++ script.js
@@ -1,14 +1,82 @@
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeapon = 0;
-let fighting;
-let monsterHealth;
-let inventory = ["stick"];
+class Player {
+  constructor(health, gold, inventory, xp) {
+    this.health = health;
+    this.gold = gold;
+    this.inventory = inventory;
+    this.xp = xp;
+  }
 
-const button1 = document.querySelector("#button1");
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
+  updateStats() {
+    healthText.innerText = this.health;
+    goldText.innerText = this.gold;
+    inventoryText.innerText = this.inventory.join(", ");
+  }
+
+  buyItem(cost, item) {
+    if (this.gold >= cost) {
+      this.gold -= cost;
+      this.inventory.push(item);
+      this.updateStats();
+      text.innerText = `You bought a ${item}.`;
+    } else {
+      text.innerText = "You do not have enough gold.";
+    }
+  }
+
+  fight(monster) {
+    // Check if the fight is already over
+    if (this.health <= 0) {
+      text.innerText = "You are too weak to fight.";
+      return;
+    }
+
+    if (monster.health <= 0) {
+      text.innerText = `You have already defeated the ${monster.name}.`;
+      return;
+    }
+
+    // Player's attack phase
+    let playerDamage = Math.floor(Math.random() * 10) + 1; // Randomize player damage
+    monster.health -= playerDamage;
+    text.innerText = `You attack the ${monster.name} and deal ${playerDamage} damage.`;
+
+    // Check if monster is defeated
+    if (monster.health <= 0) {
+      this.win(monster);
+      return;
+    }
+
+    // Monster's counterattack phase
+    let monsterDamage = Math.floor(Math.random() * 5) + 1; // Randomize monster damage
+    this.health -= monsterDamage;
+    text.innerText += `\nThe ${monster.name} counterattacks and deals ${monsterDamage} damage to you.`;
+
+    // Update stats
+    this.updateStats();
+    monsterHealthText.innerText = monster.health;
+
+    // Check player's survival
+    if (this.health <= 0) {
+      this.lose();
+    }
+  }
+}
+class Monster {
+  constructor(name, health) {
+    this.name = name;
+    this.health = health;
+  }
+}
+
+// Initialize player
+let player = new Player(100, 50, ["stick"]);
+
+// UI elements
+const buttons = {
+  store: document.querySelector("#button1"),
+  cave: document.querySelector("#button2"),
+  fight: document.querySelector("#button3"),
+};
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
@@ -18,155 +86,32 @@ const monsterNameText = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const inventoryText = document.querySelector("#inventoryText");
 
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
-
-function xpGain(num) {
-  xp += num;
-  xpText.innerText = xp;
-}
-
-function healthGain(num) {
-  health += num;
-  healthText.innerText = health;
-}
-
-function goldGain(num) {
-  gold += num;
-  goldText.innerText = gold;
-}
+// Button event listeners
+buttons.store.onclick = () => goStore();
+buttons.cave.onclick = () => goCave();
+buttons.fight.onclick = () => fightDragon();
 
 function goStore() {
+  // Store logic
   text.innerText = "You enter the store.";
-  button1.innerText = "Buy health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go home";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goHome;
-  button1.style.display = "block";
-  button2.style.display = "block";
-  button3.style.display = "block";
-  text.style.display = "block";
-  inventoryText.style.display = "none";
+  buttons.store.innerText = "Buy health (10 gold)";
+  buttons.store.onclick = () => player.buyItem(10, "health potion");
+  // More store logic
 }
 
 function goCave() {
-  text.innerText = "You enter the cave.";
-  button1.innerText = "Fight dragon";
-  button2.innerText = "Go back to store";
-  button3.innerText = "Go home";
-  button1.onclick = fightDragon;
-  button2.onclick = goStore;
-  button3.onclick = goHome;
-  button1.style.display = "block";
-  button2.style.display = "block";
-  button3.style.display = "block";
-  text.style.display = "block";
-  inventoryText.style.display = "none";
-}
-
-function buyHealth() {
-  if (gold >= 10) {
-    gold -= 10;
-    goldText.innerText = gold;
-    health += 10;
-    healthText.innerText = health;
-  } else {
-    text.innerText = "You do not have enough gold to buy health.";
-  }
-}
-
-function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    goldText.innerText = gold;
-    inventory.push("sword");
-    inventoryText.innerText = inventory;
-  } else {
-    text.innerText = "You do not have enough gold to buy a weapon.";
-  }
-}
-
-function goHome() {
-  text.innerText = "You head back to your house.";
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = "";
-  button1.style.display = "block";
-  button2.style.display = "block";
-  button3.style.display = "none";
-  text.style.display = "block";
-  inventoryText.style.display = "none";
+  // Cave logic
 }
 
 function fightDragon() {
-  fighting = 1;
-  goCave();
-  monsterHealth = Math.floor(Math.random() * 20) + 80;
-  monsterStats.style.display = "block";
-  monsterNameText.innerText = "Dragon";
-  monsterHealthText.innerText = monsterHealth;
-}
-
-function attack() {
-  text.innerText = "The Dragon attacks.";
-  health -= Math.floor(Math.random() * 5) + 1;
-  healthText.innerText = health;
-  if (health <= 0) {
-    lose();
-  }
-}
-
-function lose() {
-  text.innerText = "You lose.";
-  button1.innerText = "Restart";
-  button2.innerText = "";
-  button3.innerText = "";
-  button1.onclick = restart;
-  button2.onclick = "";
-  button3.onclick = "";
-  button1.style.display = "block";
-  button2.style.display = "none";
-  button3.style.display = "none";
-  text.style.display = "block";
-  inventoryText.style.display = "none";
-}
-
-function win() {
-  text.innerText = "You win!";
-  button1.innerText = "Restart";
-  button2.innerText = "";
-  button3.innerText = "";
-  button1.onclick = restart;
-  button2.onclick = "";
-  button3.onclick = "";
-  button1.style.display = "block";
-  button2.style.display = "none";
-  button3.style.display = "none";
-  text.style.display = "block";
-  inventoryText.style.display = "none";
+  let dragon = new Monster("Dragon", 100);
+  player.fight(dragon);
 }
 
 function restart() {
   location.reload();
 }
 
-function lose2() {
-  text.innerText = "You lose.";
-  button1.innerText = "Restart";
-  button2.innerText = "";
-  button3.innerText = "";
-  button1.onclick = restart;
-  button2.onclick = "";
-  button3.onclick = "";
-  button1.style.display = "block";
-  button2.style.display = "none";
-  button3.style.display = "none";
-  text.style.display = "block";
-  inventoryText.style.display = "none";
-}
+// Initial UI setup
+text.innerText = "Welcome to Dragon Repeller...";
+// More setup
